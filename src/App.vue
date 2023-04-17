6553140d333f3e42804176afcfc820d8f6004f54
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-04-17 16:25:50 +0200

    add axios

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,42 @@
 
 <script>
+import axios from 'axios'
 export default {
-
+  data() {
+    return {
+      titolo: 'yo-gi-ho',
+      yoGiHoUrl: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
+      gameCards: null
+    }
+  },
+  methods: {
+    callApi(url) {
+      axios.get(url)
+        .then(response => {
+          console.log(response);
+          this.gameCards = response.data
+        })
+        .catch(err => {
+          console.log(err),
+            console.log(err.message)
+        }
+        )
+    }
+  },
+  mounted() {
+    this.callApi(this.yoGiHoUrl)
+  },
 }
 </script>
 
     
 <template>
   <div>
-    <h1 class="text-uppercase">vite-yu-gi-oh</h1>
+    <h1 class=" text-uppercase"> {{ titolo }} </h1>
   </div>
 </template>
 
 <style lang="scss" scoped>
-@use '../node_modules/bootstrap/scss/bootstrap.scss';
-
 h1 {
   color: red;
 }
